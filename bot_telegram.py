cf3b6a4704361b92d90ca0101dbbc28640982bb7
Dordeks <Jdordeks@gmail.com>
2022-10-07 16:37:43 +0500

    Changes by rkd

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,7 +1,8 @@
 from aiogram.utils import executor
 from Create_bot import dp
 from aiogram import types
-from aiogram.dispatcher.filters import Text, IDFilter, BoundFilter
+from aiogram.dispatcher.filters import Text
+
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from texts import *
@@ -29,9 +30,55 @@ async def check_keyword(message: types.Message, state: FSMContext):
     else:
         await message.answer(wrong_keyword)
 
-@dp.message_handler(lambda message: "Создать опрос")
-async def create_poll(poll: types.Poll):
-    await
+@dp.message_handler(Text(equals='Создать опрос', ignore_case=True))
+async def create_poll(message: types.Message):
+    await message.answer(createpoll_is_anonymous, reply_markup=yes_no_kb)
+    await CreatePollState.is_anonymous.set()
+
+
+@dp.message_handler(state=CreatePollState.allows_multiple_answers)
+async def poll_allows_multiple_answers(message: types.Message, state: FSMContext):
+    if message.text == "Да":
+        await state.update_data(allows_multiple_answers=True)
+        await message.answer(createpoll_question, reply_markup=remove_kb)
+        await state.set_state(CreatePollState.question)
+    elif message.text == "Нет":
+        await state.update_data(allows_multiple_answers=False)
+        await message.answer(createpoll_question, reply_markup=remove_kb)
+        await state.set_state(CreatePollState.question)
+    else:
+        await message.answer("Выберите вариант из предложенных")
+
+@dp.message_handler(state=CreatePollState.question)
+async def poll_question(message: types.Message, state: FSMContext):
+    await state.update_data(question=message.text)
+    await message.answer(createpoll_answers)
+    await state.set_state(CreatePollState.answers)
+    await state.update_data(answers=[])
+
+
+@dp.message_handler(Text(equals='Добавить новый ответ', ignore_case=True), state=CreatePollState.answers)
+async def poll_answers_add_another(message: types.Message, state: FSMContext):
+    await message.answer(createpoll_answers_2, reply_markup=remove_kb)
+
+@dp.message_handler(Text(equals='Закончить с ответами', ignore_case=True), state=CreatePollState.answers)
+async def poll_answers_end(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    await state.finish()
+    await message.answer_poll(question=data["question"], options=data["answers"], is_anonymous=data["is_anonymous"],
+                              allows_multiple_answers=data["allows_multiple_answers"])
+
+
+@dp.message_handler(state=CreatePollState.answers)
+async def poll_answers(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    answers = data["answers"]
+    answers.append(message.text)
+    if len(answers) < 2:
+        await message.answer(createpoll_answers_1)
+    else:
+        await message.answer(reply_markup=create_new_or_not)
+    state.update_data(answers=answers)
 
 
 
--- states.py
+++ states.py
@@ -1,8 +1,12 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-
-
 class StartKeywordState(StatesGroup):
     waiting_for_keyword = State()
 
+class CreatePollState(StatesGroup):
+    is_anonymous = State()
+    question = State()
+    answers = State()
+    allows_multiple_answers = State()
+    expire_time = State()
 
